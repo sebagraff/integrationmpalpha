f6cb799a20c060472fe284ac7ac37320b16b452f
sebagraff <sebastiandavidgraff@gmail.com>
2021-11-02 22:08:56 -0300

    java version

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -2,40 +2,40 @@
 
 
 const app = Vue.createApp({
-    data() {
-        return {
-            products: [],
-            buy: {},
-        }
+  data() {
+    return {
+      products: [],
+      buy: {},
+    }
 
-    },
+  },
 
-    created() {
-        axios.get('/api/products')
-            .then(res => {
-                this.products = res.data
-                console.log("holamundo")
-            }).catch(res => console.log("bye world"))
-            
-        },
+  created() {
+    axios.get('/api/products')
+      .then(res => {
+        this.products = res.data
+        console.log("holamundo")
+      }).catch(res => console.log("bye world"))
 
-    methods: {
-        buyPhone(phones){
-            console.log(phones.id)
-            var myHeaders = new Headers();
-            myHeaders.append("x-integrator-id", "dev_24c65fb163bf11ea96500242ac130004");
-            myHeaders.append("Authorization", "Bearer APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-            myHeaders.append("Content-Type", "application/javascript");
-            
-            var raw =`{
+  },
+
+  methods: {
+    buyPhone(phones) {
+      console.log(phones.id)
+      var myHeaders = new Headers();
+      myHeaders.append("x-integrator-id", "dev_24c65fb163bf11ea96500242ac130004");
+      myHeaders.append("Authorization", "Bearer APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+      myHeaders.append("Content-Type", "application/javascript");
+
+      var raw = `{
                 "items": [
                   {
-                      "id": "`+phones.id+`",
-                    "title": "`+phones.name+`",
-                    "description": "`+phones.description+`",
-                    "picture_url": "`+phones.imgUrl+`",
-                    "quantity": `+phones.stock+`,
-                    "unit_price": `+phones.price+`,
+                      "id": "`+ phones.id + `",
+                    "title": "`+ phones.name + `",
+                    "description": "`+ phones.description + `",
+                    "picture_url": "`+ phones.imgUrl + `",
+                    "quantity": `+ phones.stock + `,
+                    "unit_price": `+ phones.price + `,
                   }
                 ],
                 "payer": {
@@ -73,23 +73,23 @@ const app = Vue.createApp({
                  "external_reference": "sebastiandavidgraff@gmail.com",
 
               }`;
-            
 
-            var requestOptions = {
-              method: 'POST',
-              headers: myHeaders,
-              body: raw,
-              redirect: 'follow'
-            };
-            
-            fetch("https://api.mercadopago.com/checkout/preferences", requestOptions)
-              .then(response => response.json())
-              .then(result => window.location.href = result.init_point)
-              .catch(error => console.log('error', error));
-            
 
-        }
-    },
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch("https://api.mercadopago.com/checkout/preferences", requestOptions)
+        .then(response => response.json())
+        .then(result => window.location.href = result.init_point)
+        .catch(error => console.log('error', error));
+
+
+    }
+  },
 })
 
 
